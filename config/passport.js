0c29044b80b27ae81c26af2e8648c363e894ef4b
Priyesh Patel <safal.priyesh@gmail.com>
2018-04-04 09:11:51 +0530

    Register with pass encryption done

--- config/passport.js
+++ config/passport.js
@@ -6,23 +6,23 @@ let config = require('../config/config');
 // Setup work and export for the JWT passport strategy
 module.exports = function(passport) {
   let opts = {
-    jwtFromRequest: ExtractJwt.fromAuthHeader(),
+   // jwtFromRequest: ExtractJwt.fromAuthHeader(),
     secretOrKey: config.auth.secret
   };
 
-  passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
-      console.log('payl',jwt_payload);
-    // User.findOne({
-    //   id: jwt_payload.id
-    // }, (err, user) => {
-    //   if (err) {
-    //     return done(err, false);
-    //   }
-    //   if (user) {
-    //     done(null, user);
-    //   } else {
-    //     done(null, false);
-    //   }
-    // });
-  }));
+  // passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
+  //     console.log('payl',jwt_payload);
+  //   // User.findOne({
+  //   //   id: jwt_payload.id
+  //   // }, (err, user) => {
+  //   //   if (err) {
+  //   //     return done(err, false);
+  //   //   }
+  //   //   if (user) {
+  //   //     done(null, user);
+  //   //   } else {
+  //   //     done(null, false);
+  //   //   }
+  //   // });
+  // }));
 };
--- routes/users.js
+++ routes/users.js
@@ -9,21 +9,23 @@ const User = require('../models/user');
 
 router.post('/register',(req,res,next) => {
     var body = _.pick(req.body, ['email', 'password']);
-    var user = new User (body);
-    user.save().then((user) => {
-        res.send(user);
-    }).catch((e) => {
-        res.status(400).send(e);
+    var newUser = new User (body);
+    bcrypt.genSalt()
+    .then((salt) => {
+            //console.log(salt);
+            bcrypt.hash(newUser.password,salt)
+            .then((hash) => {
+                    //console.log(hash);
+                    newUser.password = hash;
+                    newUser.save().then((user) => {
+                        res.send(user);
+                    })
+                    .catch((e) => {
+                        res.status(400).send(e);
+                })
+            })
+    
     })
-    // bcrypt.genSalt()
-    // .then((salt) => {
-    //     //console.log(salt);
-    //     bcrypt.hash(newUser.password,salt)
-    //     .then((hash) => {
-    //         //console.log(hash);
-    //         newUser.password = hash;
-    //     })
-    // })
     // newUser.save().then((doc) => {
     //     res.json({success: true, msg: 'User registered'})
     // },(e) => {
